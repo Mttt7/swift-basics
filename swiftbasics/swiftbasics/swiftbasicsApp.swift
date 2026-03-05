82a02743571eecdfc009d31ec1e9ec475686f14b
Michał <groobytt@gmail.com>
2026-03-05 16:35:56 +0100

    Task#1 Initial Commit

---
+++ swiftbasics/swiftbasics/swiftbasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftbasicsApp.swift
+//  swiftbasics
+//
+//  Created by Michał on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct swiftbasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
